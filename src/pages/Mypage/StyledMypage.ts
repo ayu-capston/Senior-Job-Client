d11506d8aad1bbfa3886439d6ed4560e23fb2392
olive-su <1466su@naver.com>
2023-05-24 21:46:27 +0900

    Feat: 마이페이지 컴포넌트, 페이지

---
+++ src/pages/Mypage/StyledMypage.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const body = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin: 20px 140px;
+`;
+
+const content = styled.div`
+    gap: 14px 50px;
+`;
+
+export { body, content };
