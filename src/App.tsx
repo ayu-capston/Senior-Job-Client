48c26a09e2b0b423d8d0d811172ec9461ca72bd7
olive-su <1466su@naver.com>
2023-05-26 16:04:00 +0900

    Chore: craco 경로 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Mypage from './pages/Mypage/Mypage';
-import CreateLecture from './pages/CreateLecture/CreateLecture';
+import Header from '@components/Header/Header';
+import Mypage from '@pages/Mypage/Mypage';
+import CreateLecture from '@pages/CreateLecture/CreateLecture';
 
 function App() {
     return (
-        <BrowserRouter>
+        <>
             <Header />
-            <Routes>
-                <Route path='/mypage' element={<Mypage />} />
-                <Route path='/mypage/lecture/create' element={<CreateLecture />} />
-            </Routes>
-        </BrowserRouter>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/mypage' element={<Mypage />} />
+                    <Route path='/mypage/lecture/create' element={<CreateLecture />} />
+                </Routes>
+            </BrowserRouter>
+        </>
     );
 }
 
