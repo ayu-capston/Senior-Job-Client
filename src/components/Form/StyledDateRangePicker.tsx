339512957df5f48dbbaacd8f40f9a08019c9b808
olive-su <1466su@naver.com>
2023-05-28 21:16:27 +0900

    Feat: DatePicker 컴포넌트화

---
+++ src/components/Form/StyledDateRangePicker.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const DatePickerSection = styled.section`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 0px 10px;
+`;
+
+export { DatePickerSection };
