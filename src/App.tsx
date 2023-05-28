7f45cb6123284f9694173937af8dd304fd0cad93
Sugyeong Kim <dev.olivesu@gmail.com>
2023-05-28 21:15:10 +0900

    Fix: App.tsx import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Router from './router/Router';
 import GlobalStyled from './styles/Globalstyled';
 
