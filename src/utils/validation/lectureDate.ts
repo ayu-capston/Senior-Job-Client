1d7c85625ce15163981829b8fa7d14a6953f6880
olive-su <1466su@naver.com>
2023-05-28 21:17:11 +0900

    Feat: lectureDate 유효성 검사 유틸 추가

---
+++ src/utils/validation/lectureDate.ts
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import dayjs from 'dayjs';
+
+interface rstObject {
+    state: boolean;
+    msg?: string;
+}
+
+export default function lectureDate(startDate: dayjs.ConfigType, endDate: dayjs.ConfigType): rstObject {
+    if (dayjs(startDate) < dayjs()) {
+        return { state: false, msg: '시작 날짜는 현재 날짜 이후로 지정해주세요.' };
+    }
+    if (dayjs(startDate) < dayjs().weekday(7)) {
+        return { state: false, msg: '종료 날짜는 최소 7일 이상이어야합니다.' };
+    }
+    return { state: true };
+}
