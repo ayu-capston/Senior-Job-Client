ad03ab1a0c8207079e486f6ee149a11e70546db7
olive-su <1466su@naver.com>
2023-06-17 04:06:06 +0900

    Refactor: 지역명 파싱 유틸 함수로  분리

--- src/components/LectureInfo/RegionInfo.tsx
+++ src/components/LectureInfo/RegionInfo.tsx
@@ -2,6 +2,7 @@ import IconLabel from '../IconLabel/IconLabel';
 import locationIcon from '../../assets/images/icon-location.svg';
 import * as S from './StyledLectureInfo';
 import KakaoMap from '../Map/KakaoMap';
+import regionParser from '~/utils/parser/regionParser';
 
 interface RegionInfoProps {
     region?: string;
@@ -9,13 +10,13 @@ interface RegionInfoProps {
 
 const RegionInfo = ({ region }: RegionInfoProps) => {
     const defaultRegion = '';
-    const sliceRegion = (region || defaultRegion).split(' ').slice(0, 2).join(' ');
+    const sliceRegion = region || defaultRegion;
     return (
         <S.Infowrap>
             <IconLabel label='강좌 지역' iconimg={locationIcon} />
             <S.Line />
             <S.Map>
-                <KakaoMap value={sliceRegion} />
+                <KakaoMap value={regionParser(sliceRegion)} />
             </S.Map>
             <S.LocationWrap>
                 <span>상세 주소는 강좌 개설시 공개됩니다.</span>
